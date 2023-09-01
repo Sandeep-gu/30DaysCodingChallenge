ffff3463317e149043e04b4da5307492b68eab2e
sandeep-g <sandeepkg8756@gmail.com>
2023-09-01 20:08:50 +0530

    first

--- day1/reverseArray.js
+++ day1/reverseArray.js
@@ -1,1 +1,8 @@
-console.log("hii codes");
+let arr = [1,2,3,4,5,6,7]
+let arr_reverser=[]
+let length = arr.length
+for (let i=length-1; i>=0; i--){
+    arr_reverser.push(arr[i])
+
+}
+console.log(arr_reverser);
